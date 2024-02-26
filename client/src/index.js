b7bb06a08ab4bcb05a754872bee96f99569e68af
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2024-02-26 23:52:03 +1000

    Setup nextui and tailwind css.

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/styles/index.css';
+import './assets/styles/output.css';
 import App from './App';
+import { NextUIProvider } from '@nextui-org/react';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>
 );
 
