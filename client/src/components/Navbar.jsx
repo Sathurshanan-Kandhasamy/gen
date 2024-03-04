6fcf4e615351e14e7353950303587f8b1386e4d5
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2024-03-04 11:36:55 +1000

    Update navbar component.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,30 +6,91 @@ import {
   Button,
   HStack,
   Link,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
 } from '@chakra-ui/react';
 
 const Navbar = () => {
   return (
-    <Card>
+    <Card borderRadius="none">
       <Container maxW="container.xl" p="2">
         <Flex justifyContent="space-between" alignItems="center">
-          <Link href="/">
-            <Heading as="h2" size="lg">
-              GEN
-            </Heading>
-          </Link>
-          <HStack>
-            <Link href="/login">
-              <Button colorScheme="gray" variant="ghost">
-                Log in
-              </Button>
-            </Link>
-            <Link href="/register">
-              <Button colorScheme="gray" variant="outline">
-                Create account
-              </Button>
-            </Link>
-          </HStack>
+          {true ? (
+            <>
+              <Link to="/">
+                <Heading as="h2" size="lg">
+                  GEN
+                </Heading>
+              </Link>
+              <HStack spacing="15px">
+                <Button
+                  as={Link}
+                  to="/new"
+                  colorScheme="gray"
+                  variant="outline"
+                >
+                  Create Post
+                </Button>
+                <Menu>
+                  <MenuButton>
+                    <Avatar size="sm" name="John Doe" src="" />
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem as={Link} to="/john">
+                      John-Doe <br /> @john{' '}
+                    </MenuItem>
+                    <MenuDivider />
+                    <MenuItem as={Link} to="/dashboard">
+                      Dashboard
+                    </MenuItem>
+                    <MenuItem as={Link} to="/new">
+                      Create Post
+                    </MenuItem>
+                    <MenuItem as={Link} to="/readinglist">
+                      Reading List
+                    </MenuItem>
+                    <MenuItem as={Link} to="/settings">
+                      Settings
+                    </MenuItem>
+                    <MenuDivider />
+                    <MenuItem as={Link} to="/logout_confirm">
+                      Log Out
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </HStack>
+            </>
+          ) : (
+            <>
+              <Link href="/">
+                <Heading as="h2" size="lg">
+                  GEN
+                </Heading>
+              </Link>
+              <HStack>
+                <Button
+                  as={Link}
+                  to="/login"
+                  colorScheme="gray"
+                  variant="ghost"
+                >
+                  Log in
+                </Button>
+                <Button
+                  as={Link}
+                  to="/register"
+                  colorScheme="gray"
+                  variant="outline"
+                >
+                  Create account
+                </Button>
+              </HStack>
+            </>
+          )}
         </Flex>
       </Container>
     </Card>
